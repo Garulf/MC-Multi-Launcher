a3ba5d527a5b9b54e789c34c8df4600c4af0ce29
Garulf <535299+Garulf@users.noreply.github.com>
2022-01-21 04:52:42 -0500

    init

---
+++ run.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+plugindir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(plugindir)
+sys.path.append(os.path.join(plugindir, "lib"))
+sys.path.append(os.path.join(plugindir, "plugin"))
+
+from plugin.mc_multi_launcher import MCMultiLauncher
+
+if __name__ == "__main__":
+    MCMultiLauncher()
