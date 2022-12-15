27fb03751108d085c247a03d809376ac81f1f4c0
Garulf <535299+Garulf@users.noreply.github.com>
2022-12-15 00:14:48 -0500

    Detect if valid path

--- src/mc_launcher/ui.py
+++ src/mc_launcher/ui.py
@@ -2,7 +2,6 @@ from flox import Flox
 from mc_launcher.launcher import MCLauncher
 from mc_launcher.instance import Instance
 from mc_launcher.results import select_launcher, query_instances
-from mc_launcher.actions import OpenFileDialog
 
 SETTINGS_LAUNCHER_DIR = 'launcher_dir'
 
@@ -11,7 +10,6 @@ class MCMultiLauncher(Flox):
 
     def __init__(self):
         """Setup settings and create the launcher object"""
-        self.open_file_dialog = OpenFileDialog(self)
         self.launcher_dir = self.settings.get(SETTINGS_LAUNCHER_DIR)
         if self.launcher_dir:
             self.mc_launcher = MCLauncher(self.launcher_dir)
@@ -24,14 +22,20 @@ class MCMultiLauncher(Flox):
         # Check if the launcher dir is set
         if not self.launcher_dir:
             # If not, ask the user to select the launcher dir
-            return select_launcher(self)
+            return select_launcher(self, query)
         return query_instances(self, query)
 
     def set_launcher_dir(self, launcher_path: str):
-        self.settings['launcher_dir'] = str(launcher_path)
+        launcher = MCLauncher(launcher_path)
+        if launcher.executable():
+            self.settings['launcher_dir'] = str(launcher_path)
+            self.change_query(self.action_keyword, True)
+        else:
+            self.show_msg('Invalid launcher path',
+                          launcher_path, ico_path=self.icon)
 
     def default_action(self, instance_path: str):
         """This is used when a user selects a result"""
         instance = Instance(instance_path)
         self.mc_launcher.launch_instance(instance)
-        self.notify('Launching instance', instance.name)
+        self.show_msg('Launching instance', instance.name)
