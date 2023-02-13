5fd9ad90acff707d00d19e4016968dfb68036f6a
Garulf <535299+Garulf@users.noreply.github.com>
2023-02-13 03:20:24 -0500

    Open config with UTF-8 encoding

--- src/mc_launcher/config.py
+++ src/mc_launcher/config.py
@@ -1,6 +1,6 @@
 
 
 def load(path):
-    with open(path, "r") as f:
+    with open(path, "r", encoding='utf-8') as f:
         props = dict(line.strip().split('=', 1) for line in f)
     return props
